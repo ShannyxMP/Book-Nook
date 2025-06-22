7dbe7cd183c3506d4fd53015eb617b115fc10003
ShannyxMP <shannyxmp@gmail.com>
2025-06-22 13:36:56 +1000

    feat: create books and reviews tables and seed initial data

--- index.js
+++ index.js
@@ -18,10 +18,31 @@ db.connect();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let books = [
+  {
+    id: 1, // SERIAL PRIMARY KEY
+    title: "City of thieves", // VARCHAR(100)
+    author: "David Benioff", //VARCHAR(100)
+    book_cover: "https://covers.openlibrary.org/b/isbn/0452295297.jpg", // VARCHAR(255)
+    isbn: "0452295297", // VARCHAR(15) UNIQUE NOT NULL, CHECK (char_length(ISBN) = 10 OR char_length(ISBN) = 13)
+    ol_link: "https://openlibrary.org/isbn/0452295297", // VARCHAR(255)
+  },
+];
+
+let reviews = [
+  {
+    id: 1, // SERIAL PRIMARY KEY
+    rating: 4, // NOT NULL, CHECK (rating >= 0 AND rating <=5)
+    review: "Featured in the The Last of Us Part II game. It was interesting.", // TEXT
+    book_isbn: "0452295297", // VARCHAR(13)
+    date_created: "2025-06-22", // DATE NOT NULL DEFAULT CURRENT_DATE
+  },
+];
+
 app.get("/", async (req, res) => {
   console.log("You're on the homepage.");
   res.render("index.ejs");
-  console.log(await db.query("SELECT * FROM test"));
+  // console.log(await db.query("SELECT * FROM test"));
 });
 
 app.listen(port, () => {
