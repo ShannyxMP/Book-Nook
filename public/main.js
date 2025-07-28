77d209b29807ac4a7ba54aeadb61ecaba22c4b57
ShannyxMP <shannyxmp@gmail.com>
2025-07-28 17:03:51 +1000

    style: create function that randomises rotation of elements for aesthetics

--- public/main.js
+++ public/main.js
@@ -3,3 +3,8 @@ function submitIfOptionChosen(selectedElement) {
     selectedElement.form.submit();
   }
 }
+
+document.querySelectorAll(".randomRotation").forEach((element) => {
+  let rotateDegrees = Math.random() * 0.02 - 0.01;
+  element.style.setProperty("--rotation", `${rotateDegrees}turn`);
+});
